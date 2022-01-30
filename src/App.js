d4d59e8ce7fd0ce6d64552bc8a7612c33f6d3da7
nisargjagani6 <91526706+nisargjagani6@users.noreply.github.com>
2022-01-30 23:45:23 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+import './App.css';
+import Coin from './Coin'
+
+
+function App() {
+  const [coins,setCoins]= useState([]);
+  const [search,setSearch]= useState('');
+  // const [count,setCount]= useState(0);
+
+  useEffect(()=>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res=>{
+      setCoins(res.data)
+    })
+    .catch(error=>{
+      console.log(error)
+    })
+
+  })
+
+
+
+
+  const handlechange= e =>{
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins=coins.filter(coin=>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+    )
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+      <h1 className="coin-text">Search a currency</h1>
+      <form>
+        <input type="text" placeholder="Search" className="coin-input" onChange={handlechange}/>
+      </form>
+    </div>
+    <label for="pet-select">Choose Currency:</label>
+
+<select className="pet-select">
+    <option value="inr" className="inr" >inr</option>
+    <option value="usd" className="usd" >usd</option>
+    <option value="rub" className="rub">rub</option>
+</select>
+    {filteredCoins.map(coin=>{
+      return(
+        <Coin
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image} 
+          Symbol={coin.symbol} 
+          marketcap={coin.market_cap} 
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
+        />
+      )
+      })}
+    </div>
+  );
+}
+
+export default App;
